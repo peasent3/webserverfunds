2c1fc863881ea00172c9b1521d3e4c4a75760ec4
peasant3 <kinjie.supe3@gmail.com>
2023-10-10 17:21:47 -0400

    Update lab53-2.php

--- lab53-2.php
+++ lab53-2.php
@@ -1,2 +1,2 @@
-<?= $_SERVER = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0"?>
-<p><?= var_dump($_SERVER) ?></p>
+<h1>User Agent</h1>
+<p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
