5acac273c269a635dbdacea7bf20cdd68c17d344
peasant3 <kinjie.supe3@gmail.com>
2023-10-10 22:16:35 -0400

    Lab 3.3

--- lab53-2.php
+++ lab53-2.php
@@ -1,7 +1,36 @@
-<head> Lab 5
-</head>
-<body>
-<h1>User Agent</h1>
+<!DOCTYPE HTML>
 
-<p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
+<head> 
+
+    <title>Lab#5: Study Club</title>
+
+</head>
+
+<body>
+
+    <h2>User Agent</h2>
+    <p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
+
+    <h2>Study Club Register</h2>
+
+    <form action="register.php" method = "GET">
+
+
+    <p> Enter your name: <input type="text" id="name" name="name" /></p> 
+    <p> Last name: <input type="text" id="lastname" name="lastname" /></p>   
+      
+    <div> Select Subject:<br />
+    <label for="subject">Subject:</label>
+            <select id="subject" name="subject">
+                <option value="math">Math</option>
+                <option value="french">French</option>
+                <option value="science">Science</option>
+            </select>
+    </div>
+
+    <input type="submit"> 
+</form>
+
 </body>
+</body>
+
---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Registar</title>
+    </head>
+
+    <body>
+
+        <p><?= var_dump($_GET) ?></p> 
+        <p><?= var_dump($_POST) ?></p>
+        
+    </body>
+</html>
