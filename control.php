47eaf51d6b6fa911248a823fd58d57c1ea345b98
peasant3 <kinjie.supe3@gmail.com>
2023-11-25 22:27:33 -0500

    Update control.php

--- control.php
+++ control.php
@@ -1,9 +1,7 @@
 <?php
-// Assuming you have a function to control the LED, for example:
+
 function controlLED($state) {
-    // Your logic to control the LED based on the received state
-    // This could involve interacting with GPIO pins or any other method
-    // For simplicity, let's just print the state for demonstration purposes
+
     shell_exec("gpio toggle 7");
     $state = shell_exec("gpio read 7");
     echo "LED is now $state";
