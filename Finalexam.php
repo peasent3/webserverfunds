b89e5ad0e3a6826b5b6af159644fa32c0672e363
peasent3 <kinjie.supe3@gmail.com>
2024-01-12 12:50:47 -0500

    Update Finalexam.php

--- Finalexam.php
+++ Finalexam.php
@@ -3,27 +3,6 @@
 
 <html>
 
-<head> 
-    <title>Final Practical Exam</title>
-
-    <?php
-        $server = "localhost";
-        $username = "bluenix";
-        $password = "1z3ass5z6p";
-        $database = "register";
-        $conn = mysqli_connect($server, $username, $password, $database);
-
-        //Connection checker
-        if (!$conn)
-        {
-            die("Connection failed: {mysqli_connect_error()}");
-            echo "Connected successfully";
-        }
-        $sql = "select * from fish;";
-        $result = mysqli_query($conn, $sql);
-    ?>
-</head>
-
 <body>
 
         <h1>Google</h1>
