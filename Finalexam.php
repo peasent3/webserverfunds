b192e5b96ae7d6036da3fe787c0d275f3e1f4b98
peasent3 <kinjie.supe3@gmail.com>
2024-01-12 11:19:38 -0500

    test
    test

---
+++ Finalexam.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE HTML>
+
+<head> 
+    <title>Final Practical Exam</title>
+
+    <?php
+        $server = "localhost";
+        $username = "bluenix";
+        $password = "1z3ass5z6p";
+        $database = "register";
+        $conn = mysqli_connect($server, $username, $password, $database);
+
+        //Connection checker
+        if (!$conn)
+        {
+            die("Connection failed: {mysqli_connect_error()}");
+            echo "Connected successfully";
+        }
+        $sql = "select * from fishing;";
+        $result = mysqli_query($conn, $sql);
+    ?>
+</head>
+
+<body>
+
+        <h1>Modify Participant Age</h1>
+      
+        <form action="Finalresponse.php" method="get">
+            <select id="search" name="search">
+            <?php
+                echo $_SERVER['REMOTE_ADDR'] 
+
+                foreach($result as $row) 
+                    { 
+                        echo "<option value='{$row['id']}'>{$row['search']}</option>\n";
+                    }
+                        mysqli_close($conn);
+            ?>
+
+            <input type required id="search" name="search"><br>
+            <input type="submit"> 
+        </form>
+
+</body>
+
+
+
---
+++ Finalresponse.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>Lab7 Modfiy Info</title>
+
+    <?php
+        $id = (int)($_GET["participantname"]);
+        $change = (int)($_GET["participantage"]);
+        $server = "localhost";
+        $username = "bluenix";
+        $password = "1z3ass5z6p";
+        $database = "register";
+        $conn = mysqli_connect($server, $username, $password, $database);
+ 
+        //Connection checker
+        if (!$conn)
+        {
+            die("Connection failed: {mysqli_connect_error()}");
+            echo "Connected successfully";
+        }
+        $sql = "update fishing set client_ip = {$change} where id={$id};";
+        $result = mysqli_query($conn, $sql);
+
+        $sql = "update fishing set search = {$change} where id={$id};";
+        $result = mysqli_query($conn, $sql);
+
+        $sql = "select * from  fishing where id = {$id};";
+        $result = mysqli_query($conn, $sql);
+    ?>
+    
+</head>
+
+</html>
